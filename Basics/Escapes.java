fc39e4ba125fb275634770805088a80d30e65e1b
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-25 00:11:00 +0530

    Escapes(P=2)

---
+++ Basics/Escapes.java
@@ -0,0 +1,10 @@
+public class Escapes {
+    public static void main(String[] args) {
+        System.out.println("Mahendra\nhello\n How are you?");
+        // \n for next lines
+        System.out.println("Mahendra\thello\t");
+        // \t for tab space that consist 8 spaces
+        System.out.println("Mahendra\saHello");
+        //\s for single space
+    }
+}
