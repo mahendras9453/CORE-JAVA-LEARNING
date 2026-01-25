37e95123c7f3fce0885f05a1093bbf7d200787b8
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-25 14:14:07 +0530

    Escapes3(P=4)

---
+++ Basics/Escapes3.java
@@ -0,0 +1,10 @@
+public class Escapes3 {
+    public static void main(String[] args){
+        System.out.println("'Core Java'");
+        // or
+        System.out.println(" \' Core Java\'");
+        System.out.println("\"Information Technology\"");
+        // But we can not write like above
+
+    }
+}
