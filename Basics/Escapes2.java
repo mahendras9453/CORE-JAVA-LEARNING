9434012ac5cddc79146c4dca901ffa10ff870b64
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-25 11:40:55 +0530

    Escapes2(P=3)

---
+++ Basics/Escapes2.java
@@ -0,0 +1,21 @@
+public class Escapes2 {
+    public static void main(String[] args) {
+        /*escapes in the java
+        \n : for next line
+        \r : carriage return
+        \t : tab space that consist 8 spaces but remember that count from started and if 8 chr count completed then count next 8 character
+        \s : single space
+        \f : form feed
+        \b : back space
+
+        \' : '
+        \" : "
+        \\ : \
+
+         */
+        // Better under standing of the \t
+        System.out.println("Name\t:Mahendra\nAge\t\t:21\nCourse\t:B tech");
+        // here Intellij Idea 4 spaces are their in \t tab spaces
+
+    }
+}
