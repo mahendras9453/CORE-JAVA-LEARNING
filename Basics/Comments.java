27e818a207bdb25366b3c9e5bcb49c258b899d86
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-26 16:40:04 +0530

    DiffComments(P=6)

---
+++ Basics/Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+    public static void main(String[] args) {
+        // 1. Single-line comment: starts with //
+        System.out.println("This is a single-line comment example");
+
+        /*
+         2. Multi-line comment: starts with
+        You can write multiple lines here.
+        Useful for longer explanations.
+        */
+        System.out.println("This is a multi-line comment example");
+
+        /**
+         3. Documentation comment (Javadoc)
+        Used to describe classes, methods, or fields.
+                Tools like Javadoc can generate HTML documentation from these.
+        */
+        System.out.println("This is a documentation comment example");
+    }
+}
