d57fb4f7132525f9346bef3ab0217d615ddc4eb1
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-26 22:26:32 +0530

    Identifier(P=7)

---
+++ Basics/Identifier.java
@@ -0,0 +1,16 @@
+public class Identifier {
+    public static void main(String[] args) {
+        /*
+        when we  talk about identifier :
+        identifier are class name , variable name,method name are these are known as
+        identifier
+        when they gts missed or anything wrong compiler show compile time error
+        and  show  <identifier> is missing.
+         */
+        int A=6;
+        //  A is identifier.
+        // identifier  class name also identifier
+        System.out.println(A);
+
+    }
+}
