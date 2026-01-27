8b5ac29bb14f364adb9aa8b882ed607f2015f25c
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-27 23:16:58 +0530

    Typesofdatatypes(P=10)

---
+++ DataTypes/Types_of_datatypes.java
@@ -0,0 +1,35 @@
+public class Types_of_datatypes {
+   public static void main(String[] args) {
+     /*
+       Types of the Data types in Java
+       1: Primitive Data types  (8)  also known as (base types)
+       2: Referenced Data types  (2+6)  also known as (Derived types)
+     - Primitive Data types  are used for the creating a variable for soring
+       single value .
+       A variable is a single value memory.
+     - Referenced data types are used for creating an object for storing the values .
+       An object is a multiple value  memory .
+                                    Primitive Datatypes                                         Referenced DataTypes
+                         Numeric                          Non-numeric                              -Array
+            Integral              Floating Point         -boolean                                  -class
+       Integer      Character         -float(4)       (boolean size not specified)             ( rest are in coming program)
+       -byte(1)      -char(2)         - double(8)
+       -short(2)
+       -int(4)
+       -long(8)
+
+
+      */
+       byte a =11;
+       int b=5000000;
+       short c=1000;
+       long d=1000000000;
+       char yes='a';
+       boolean No=false;
+       double g= 0.6;
+       float n=9.67F;
+
+       System.out.println(n);
+
+    }
+}
