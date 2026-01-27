8342260ad8070ab7be9c67417b379233de735c98
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-27 20:44:57 +0530

    Aboutdatatypes(P=9)

---
+++ DataTypes/datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes {
+    /*
+    what is Data Types?
+    - A keyword or class name that us used for allocating memory
+    for storing value in a program is called data types.
+    why data types?
+     - A data types is used for creating a variable or an object for storing
+     single value or multiple value .
+     */
+    public static void main(String[] args){
+        //10       CE       <     ; expected
+        //  10;    CE       <      Not a statement
+        //a=10;      CE       <      Can not find symbol
+       int a=10;   // No error    Because the  variable with the name a is created and
+                                  // value 10 is stored in the Variable a
+        System.out.println(a);
+    }
+}
