e5ca2b4d7aa0037fe942e382c11b3eaeb69aac83
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-29 20:10:17 +0530

    ProblemofVariable(P=12)

---
+++ DataTypes/Problem_of_variable.java
@@ -0,0 +1,12 @@
+public class Problem_of_variable {
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println("x is"+x);
+        x=20;
+        System.out.println("x ix"+x);
+        /*
+        the problem is  you can see above the value x is overwrite the value of x exist.
+        so we can see that the drawback of it is we can  not store the multiple values.
+         */
+    }
+}
