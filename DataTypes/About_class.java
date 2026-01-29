00f490c6b110a23a1fd0f0e211c94cb7703402e2
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-29 20:12:58 +0530

    aboutclass(P=14)

---
+++ DataTypes/About_class.java
@@ -0,0 +1,19 @@
+public class About_class {
+    public static void main(String[] args) {
+        /*
+         class is type of referenced Datatype of an object.
+         */
+        About i5 = new About();
+        System.out.println(i5.i2);
+        System.out.println(i5.i4);
+
+
+    }
+     static class About{ // for storing purpose
+        int i1=10;
+        int i2=20;
+        long i3=10393709278L;
+        char i4='a';
+
+    }
+}
