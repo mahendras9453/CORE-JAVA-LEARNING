20d1143d457f3d08804dd92a31c2b8d9b63db81e
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-31 15:54:41 +0530

    DiffWaystorealvalue(P=16)

---
+++ Runtime/Ways_readvalue.java
@@ -0,0 +1,18 @@
+public class Ways_readvalue {
+    public static void main(String[] args) {
+        /*
+        In the java supports 10 ways to connect to KB and read values from end_user from KB
+        1: From command line
+        2: By using System.in
+        3; By using Data input stream
+        4: By using Buffer reader Class
+        5: By using Scanner class
+        6: By using console class
+        7: By using System properties
+        8: By using System AWT
+        9: By using Swing window
+        10: By using Javafx
+
+         */
+    }
+}
