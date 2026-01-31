10734ba991e4425c408aab72e68929d4f019d443
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-31 16:01:13 +0530

    Readmethod(P=19)

---
+++ Runtime/Read_method.java
@@ -0,0 +1,12 @@
+
+import java.io.IOException;
+
+public class Read_method {
+    public static void main(String[] args)  throws IOException {
+        System.out.println("Enter any nUmber ");
+        int a=System.in.read();
+        System.out.println(a);//input a and output is 97
+        //So its give ASCII value
+
+    }
+}
