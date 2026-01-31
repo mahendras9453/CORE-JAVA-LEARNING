6670a100db129588234e6f0b07c58f9b0e7e2ac6
mahendras9453 <patelmahendra9152@gmail.com>
2026-01-31 15:57:35 +0530

    CommandMethod(P=17)

---
+++ Runtime/Command.java
@@ -0,0 +1,9 @@
+public class Command {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        /*
+        after compilation when we give java args 10 or 7.5 or john and enter to run the program
+        then output will be 10 or 7.5 or john
+         */
+    }
+}
