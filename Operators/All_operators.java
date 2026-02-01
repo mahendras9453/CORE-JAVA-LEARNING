f8529ee6cf2e033ddc442971b373c56e92a4a3a5
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-02 01:02:19 +0530

    Alloperators(P=25)

---
+++ Operators/All_operators.java
@@ -0,0 +1,19 @@
+public class All_operators {
+    public static void main(String[] args) {
+        /*
+          1) Assignment Operator (1)      ......: =
+          2) Arithmetic Operator (5)     .......:  _ + * / %
+          3) Increment & Decreement Operator(2).:  ++ --
+          4) Relational Operator    (5)      ...:  < > <= >=  instances
+          5) Equality Operator  (2)        .....: == !=
+          6) Logical Operator  (3)         .....: && || !
+          7) Bitwise Operator  (4)         .....: & | ^ ~
+          8) Shift Operator    (3)         .....: << >> >>>
+          9) Compound Operator  (11)       .....: += -= *= /= %= &= |= ^=
+                                                  <<= >>= >>>=
+          10) Object creation Operator (1) .....: new
+          11) Ternary operator    (2)       ....: ?  :
+          12) Lambda  Operator   (1)        .....: ->
+         */
+    }
+}
