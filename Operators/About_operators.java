af966483357dbaed4a18bd85da6276a5022162b4
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-02 01:00:25 +0530

    AboutOperators(P=24)

---
+++ Operators/About_operators.java
@@ -0,0 +1,19 @@
+public class About_operators {
+    public static void main(String[] args) {
+        /*
+        In java there are 40 operators .
+        and 39 operator are available from java 1.0
+        and one operator has been added in java 8 lambda operator
+        >> Based on the functionality of an operator w
+           we have 3 types of operators
+           1) Assignment operator ....: =
+           2) Validation Operator  ....: < > <= >= == != && || etc..
+           3) calculation operator .....:  _ + * % & | etc..
+         >> Based on operator accepted by an operator  are divide into 3 types
+         1) Unary Operator (11)
+         2) Binary Operator (27)
+         3) Ternary Operator (2)
+
+         */
+    }
+}
