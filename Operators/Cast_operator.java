0b142600518994c912ebd64c96aa2ece50d18127
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-02 20:56:50 +0530

    CastOperator(P=26)

---
+++ Operators/Cast_operator.java
@@ -0,0 +1,11 @@
+public class Cast_operator {
+    public static void main(String[] args) {
+        /*
+         cast operator is used to convert higher range to lower range or
+          lower to higher range in compatible data types.
+         */
+        int a=(int) 10.6;
+         //int b =(int)true; compile time error  not compatible.
+        System.out.println(a);
+    }
+}
