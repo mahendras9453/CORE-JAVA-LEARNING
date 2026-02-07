f44d79d61fdb1021d33c4d327468eb924b163d76
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-07 23:00:56 +0530

    ShiftOP(P=37)

---
+++ Operators/Shift_op.java
@@ -0,0 +1,11 @@
+public class Shift_op {
+    public static void main(String[] args) {
+        /*
+        Shift Operator    (3)         .....: << >> >>>
+         */
+        int a=8;
+        System.out.println(a<<2); // 32 it will add two bits at end
+        System.out.println(a>>2);//2 it will remove ast bit
+        System.out.println(a>>>2);//fills with 0s
+    }
+}
