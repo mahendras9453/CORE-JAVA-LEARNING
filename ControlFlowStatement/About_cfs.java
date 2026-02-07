cd9e6fa1b1348ca66518b663fb0a9b768f669468
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-07 23:02:35 +0530

    AboutCfs(P=38)

---
+++ ControlFlowStatement/About_cfs.java
@@ -0,0 +1,23 @@
+public class About_cfs {
+    public static void main(String[] args) {
+        /*
+         CFS
+         A keyword is used to control the flow of execution o=is technically called control flow statements.
+          It is used for taking decision to execute a block of statements either  0/1/multiple times depending on the given input
+           How many CFS does java supports?
+            those 11 keyword are organized into 3 groups.
+            they are:
+             1) conditional or Decision-making  statements (5)
+             2) Loop statements (3)
+             3) Branching / Transfer statements (3)
+            - Under Conditional statements have 5 keywords they are if, else, switch ,case,default.
+             -All these conditional statements  are used for taking decision to execute a block of statements either 0 or 1 time
+             - under loop CFS we have 3 keywords
+              - they are while,do...while,for.
+               -All those loop cFS are used for taking  decision to execute a block of statements either 0 or n
+             - number times until condition is false.
+             - under branching CFS we have 3 keywords
+              - they are break,continue and return;
+         */
+    }
+}
