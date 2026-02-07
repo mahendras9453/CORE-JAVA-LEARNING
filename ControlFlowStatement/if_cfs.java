b19882a4dbac148715db17464b43a9ead1d0ef73
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-07 23:04:04 +0530

    IfCfs(P=39)

---
+++ ControlFlowStatement/if_cfs.java
@@ -0,0 +1,14 @@
+public class if_cfs {
+    public static void main(String[] args) {
+        /*
+        if(boolean condition){
+            statement
+        }
+         */
+        int a=10;
+        //if(a=10){ not allowed
+        if(a==10){
+            System.out.println("hello");
+        }
+    }
+}
