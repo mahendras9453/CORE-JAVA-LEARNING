8c890631c033d3e7f94fd45d0f317199af4fe76b
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-08 10:52:01 +0530

    forloop(P=45)

---
+++ Loops/for_loop.java
@@ -0,0 +1,26 @@
+public class for_loop {
+    public static void main(String[] args) {
+        /*
+        fyntax
+        for( var int ; boolean cond ;var incr/decr){
+        -----------------------------
+        ---------------------------
+        ----------------------------
+        }
+        if we need to prepare loop
+        1) by a variable nased condition by var  de, intiti,condition, inc/dec all by our own and
+        2) if we know how many times loop statements must be iteratted
+        3) if we want to prepare a loop to execute for some range of values, then we must use for loop
+
+         */
+        for (int i = 1; i <=5; i++) {
+            System.out.println("mahendra");
+
+        }
+        // inner or nested loop
+        /*
+              A loop that is created another loop is called nested loop
+              that inner loop ca nbe any of the three loop
+         */
+    }
+}
