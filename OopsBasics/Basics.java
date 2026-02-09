60b1ca862668dc32ad5cf9c84322ee6e0f4dbb0b
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-09 19:17:55 +0530

    AboutOops(P=48)

---
+++ OopsBasics/Basics.java
@@ -0,0 +1,33 @@
+public class Basics {
+    public static void main(String[] args) {
+        /*
+          Object Oriented Programming(OOP
+          1) Basics( class , Object,instance)
+          2) Models(SC ->MIs and SC-> MCs->MIs)
+          3) Relations (IS-A, HAS-A,USES-A,O-O,O-M,M-O,M-M0
+          4) Principles (Enc,inh,Poly,Abs,super,VH,MH,OR,OL,etc..)
+          5) Java 5, 8, and 10 new features
+          6) Coupling Cohesive and there  types [S.O.L.I.D Principles]
+          7) LC-RP architecture & Reflection API
+          8) Sample project Development by using oop concepts
+          what is oops?
+          oops, is a  technique .It provides set of suggestion for creating real-world object in programming
+           world by achieving security and dynamic binding.
+           why oops?
+           1) To create real - worlds object in programming world
+           2) To achieve security to object data
+           3) to achieve dynamic binding between objects communication.
+            diff Programming elements to bring Real world object into Programming world
+            1) module     -     meant for grouping packages
+            2) package    _     meant for grouping classes
+            3) class      _      meant for representing object
+            4) variable   _      meant for allocating memory
+            5) block      _      meant for storing values in variables
+            6) constructor _     meant for storing values in variables
+            7) method      _     meant for implementing operations
+            8) inner class _     meant for representing inner object of an outer object
+
+         */
+
+    }
+}
