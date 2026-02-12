c8c858b3b8cebec1d470b43f0c15416d0dd0174d
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-12 23:54:02 +0530

    aboutmethods(P=56)

---
+++ Oopsmethods/about_method.java
@@ -0,0 +1,37 @@
+public abstract class about_method {
+    /*
+        A sequence of statement placed as one group with proper name and return type us called method.
+        How to create a method?
+        to create method you should know-
+        -What is an operation?
+        -What are the inputs needed to perform this operation?
+        - What is the logic to perform this operation?
+        - what is type of result returning form thus operation?
+        - what is the exception throwing from this operation?
+        - what are the permissions we must set to access and execute this operation?
+
+     */
+    //Develop a program to create a method with full syntax to take a number and strings as  arguments , concat the and print result
+
+    /* public  static void con(int a,String b) {
+         System.out.println(a + b);
+     }
+
+     */
+
+     /*
+     Method terminology
+     1) prototype -- Head of the method is known as prototype that contains modifiers, return type,methodname,
+                     , parameter and throws exception list is called prototype
+     2) Body and logic--- just the place between {} is called body.
+                          The statement placed in the method is called logic
+     3) Signature----------  Method name and parameters list together we called signature
+     4) Parameter and Argument--- The variable that is declared in method parenthesis () is called parameter
+                                  The value we are passing as input to a method parameter is called argument
+     5) Method name------    The name of a method that represents an operation performed by this is called method name
+     6) Return type--------A data type placed in the method protype before method name is called return type.
+     7) Modifiers----------  A key word or set of keywords those modifiers default .behaviour or operation of a programming element is called modifier
+                           The AM specifies where we can access this Pe and ELM specifies we can access and how we can executes his PE
+                           either wih object or directly by class name.
+      */
+}
