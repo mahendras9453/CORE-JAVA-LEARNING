474e908802f56256215e4997c5044be4f60b46aa
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-12 20:29:33 +0530

    Addition(P=54)

---
+++ Oopsvariable/Addition.java
@@ -0,0 +1,22 @@
+public class Addition {
+    public static void main(String[] args) {
+        add(5,6);
+    }
+    static void add(int a , int b){
+        int c=a +b;
+        System.out.println(c);
+        /*Final conclusion
+                Based of data  types we have two types of variable for storing a value and an object refernce.
+             1)Primitive data type
+             2)Referenced datatype
+             Both are of two types
+             1)class level var
+            2) method level var
+             class  level var are divided into two types
+             static and non-static
+              method  level var are divided into two types
+              parameter and local
+               all four types of var can be primitive and referenced.
+         */
+    }
+}
