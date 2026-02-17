715477a79cb51c4e2a8934177e073e9f6b8fb419
mahendras9453 <patelmahendra9152@gmail.com>
2026-02-17 11:22:29 +0530

    fields(P=62)

---
+++ Oopsmethods/fields.java
@@ -0,0 +1,23 @@
+public class fields {
+    /*
+    Binding fields and methods
+    1. Binding means connecting
+       The process of accessing fields (SV an NSV ) of a class
+       from its methods is called binding fields and methods
+       2. The need of accessing / binding fields with methods is in project we must perform 4
+        operations
+        a) Initializing object
+        b) reading object data
+        c) Modifying object data
+        d) Printing object data
+        repeatedly for every object created from a class
+       3. To perform above four operations
+           For developing logic one time and to reuse that
+           logic repeatedly for very object , we must bind variables and methods .
+
+     */
+
+    //We write  so that the values passed into the method are stored in the object’s fields.
+    // Without , if parameter names match field names, the assignment would fail to update the object.
+
+}
