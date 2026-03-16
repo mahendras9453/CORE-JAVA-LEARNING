d4d0b0670b068976806d4cd424e4c3d89426505d
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-16 09:40:47 +0530

    setterVSconstructor(P=68)

---
+++ Oopsmethods/setterVscons.java
@@ -0,0 +1,11 @@
+public class setterVscons {
+    /*
+    which approach is best ?
+    setter method approach is the best approach
+   1) can be executed multiple times for the same object
+   2) Using setter method we can initialize, and also we can modify object properties
+   3) we can define separate setter for every property, and we have choice to call setter method based on the property , and we can
+      ignore some of the property's initialization.
+    4) no need to use local variable for reading initial values from KB
+     */
+}
