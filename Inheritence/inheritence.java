64c2f167db7b4c8d87d443f6aba57d84f31a1e8e
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-21 18:46:15 +0530

    Inheritence(P=76)

---
+++ Inheritence/inheritence.java
@@ -0,0 +1,45 @@
+public class inheritence {
+    /*
+          What is inheritance
+          An inheritance is one of the OOp principles
+           I is a Parent - Child relation
+
+           The process of creating a new class or an object deriving from an existing class or an object for obtaining the existing object's type,
+           properties and behaviours to this new object by adding new properties and behaviours as per this new object is called inheritance
+
+           why inheritance ?
+            Inheritance is meant for either 'extending or implementing ' the functionaly of an object
+            by creating new class as sub class
+             if our target ' just reusing ' do not develop inheritance , you access the members of that existing class by creating object of that class
+
+
+             When we must develop inheritance ?
+              we must develop inheritance in two situations
+              1. For extending the functionality of an object by creating new class as sub class
+              2. For creating multiple object if same type as one group we must develop inheritance
+         How can we decide wheather to develop inheritance or not ?
+          _ Apply IS-A test on this new object
+          _ For Example
+          1) Lion IS-A What?
+            - animal
+            _ first create a class called Animal
+            then we create class Lion as a subclass of Animal
+            2) Vehicle IS-A what?
+             - Vehicle
+             - Just create a class with name, vehicle , no inheritance
+
+             How we can develop inheritance ?
+               1. either by using 'extends' keyword
+               2.or by using 'implements' keyword
+
+
+           when must use extend and implements?
+               we must use extends either
+                 - for creating a 'class' deriving from another class or
+                 - for creating new interface deriving from other interface
+              ... we must use implements
+               - for creating a class deriving from an interface
+
+
+     */
+}
