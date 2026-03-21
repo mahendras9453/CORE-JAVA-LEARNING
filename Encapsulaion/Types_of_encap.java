7f8c4be23b150e54cb72ea5be5791ea13b2dd2d2
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-21 09:25:28 +0530

    TypesofEncapsulation(P=75)

---
+++ Encapsulaion/Types_of_encap.java
@@ -0,0 +1,21 @@
+public class Types_of_encap {
+    /*
+    Types of encapsulation
+    1. Weak encapsulation
+    class Example{
+     int x;
+     private int y;
+     }// example of weak encapsulation
+
+
+    2.Strong encapsulation
+      class Example{
+      private int x;
+     private int y;
+     }// example of strong encapsulation
+       class Example{
+      final int x=10;
+     private int y=60;
+     }// example of strong encapsulation
+     */
+}
