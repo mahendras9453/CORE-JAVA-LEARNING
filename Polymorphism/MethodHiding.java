90ef1a85df40b62253e9d15740510c8a84796762
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-22 10:26:40 +0530

    MethodHiding(P=78)

---
+++ Polymorphism/MethodHiding.java
@@ -0,0 +1,16 @@
+public class MethodHiding {
+    /*
+    what is  the method hiding?
+     Hiding is flavour of Method  overriding,
+     if the overriding method is a static method we must call it as method hiding.
+     It means providing new implementation to static method in sub class as per sub class is called method overring
+     for ex:
+     classA{                           class B extends A{
+
+     static void m1(){}  >>>>> MH >>  static void mi(){}
+     Rule:
+     We can not Hide or Override a method in same class .
+     We can only Hide or override a method in subclass
+      we can over load a method in the same clas or in sub class
+     */
+}
