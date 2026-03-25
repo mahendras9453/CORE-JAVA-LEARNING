f908e8b9b59bb59270c333d620b07f1902a37808
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-25 20:02:20 +0530

    Polymormishmrules(P=80)

---
+++ Polymorphism/polyrule2.java
@@ -0,0 +1,11 @@
+public class polyrule2 {
+    /*
+     A a1= new B();
+     a1.m1();
+     >> Compiler an JVM will search and executes static method logic ref var class.
+      In above example , if m1() is static method , it isd searched and executed from class A
+      ...>> compiler and JVM will search NSm declaration in ref var class , but executes its logic from CO class.
+      In above example , if m1() is non static method , it is searched in class A and executes in class B
+     */
+
+}
