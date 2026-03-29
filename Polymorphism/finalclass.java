44f114775eb544cd971cc7142fd116f94a4ba6af
mahendras9453 <patelmahendra9152@gmail.com>
2026-03-29 16:24:37 +0530

    Finalclass(P=92)

---
+++ Polymorphism/finalclass.java
@@ -0,0 +1,27 @@
+public class finalclass {
+    /*
+     A class that is declared as final is called final class
+     when we do not want to  create sub classes from this class we must
+      declare this class as final
+       when we want stop inheritance from this class we must declare this class a final
+
+       Rule on final class
+       ::::: We can not create sub class
+
+        final class Example{
+        }
+         class Sample extends Example{
+         }
+         // above code give CE can ni=ot inherit
+
+          Q ) can we create all 10 static and non-static members in final class ?
+                 >>>>>>YES
+         Q)  CAn we instantiate final class and access it members?
+             YEs
+          Q)  if we declare class a final , ae its memebrs final ?
+            >>>>>>NO
+
+
+     */
+
+}
